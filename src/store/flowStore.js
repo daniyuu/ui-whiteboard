a060b88fd37aa422919b8b3b372839aeb86c6079
Luna Chen <yueyingchen@microsoft.com>
2024-09-05 09:04:47 +0800

    add getAnswer action

--- src/store/flowStore.js
+++ src/store/flowStore.js
@@ -1,174 +1,186 @@
 import _, { remove } from "lodash";
 import { defineStore } from "pinia";
-import { getWhiteBoardById, updateWhiteBoard, getNewFormNodes, getNewSearchNodes, getNewAINodes } from "../api";
+import {
+  getWhiteBoardById,
+  updateWhiteBoard,
+  getNewFormNodes,
+  getNewSearchNodes,
+  getNewAINodes,
+  getAnswer,
+} from "../api";
 import { nanoid } from "nanoid";
 
 const parserData = (data) => {
-    const nodes = _.get(data, "data.graph.nodes", []).map((node) => {
-        return {
-            id: node.id,
-            updated_at: node.updated_at,
-            created_at: node.created_at,
-            ...node.ui_attributes
-
-        }
-    })
-    const edges = []
-    console.log(nodes)
+  const nodes = _.get(data, "data.graph.nodes", []).map((node) => {
     return {
-        nodes,
-        edges
-    }
-}
+      id: node.id,
+      updated_at: node.updated_at,
+      created_at: node.created_at,
+      ...node.ui_attributes,
+    };
+  });
+  const edges = [];
+  console.log(nodes);
+  return {
+    nodes,
+    edges,
+  };
+};
 
 export const useFlowStore = defineStore("flow", {
-    state: () => {
-        return {
-            id: "",
-            nodes: [
-            ],
-            edges: [],
-            recommendNodes: [
-            ],
-            originalData: {},
-            name: "",
-            avatar: ""
-        }
+  state: () => {
+    return {
+      id: "",
+      nodes: [],
+      edges: [],
+      recommendNodes: [],
+      originalData: {},
+      name: "",
+      avatar: "",
+    };
+  },
+  actions: {
+    setId(id) {
+      this.id = id;
     },
-    actions: {
-        setId(id) {
-            this.id = id
+
+    async getFlow() {
+      const data = await getWhiteBoardById(this.id);
+      const { edges, nodes } = parserData(data);
+      this.nodes = nodes;
+      this.edges = edges;
+      this.originalData = data;
+      this.name = data.name;
+      this.avatar = data.avatar;
+      console.log(edges, nodes, data);
+    },
+
+    async saveFlow(avatar) {
+      if (avatar) {
+        this.avatar = avatar;
+      }
+      const data = {
+        name: this.name,
+        ui_attributes: {
+          avatar: this.avatar,
         },
-
-        async getFlow() {
-            const data = await getWhiteBoardById(this.id)
-            const { edges, nodes } = parserData(data)
-            this.nodes = nodes
-            this.edges = edges
-            this.originalData = data
-            this.name = data.name
-            this.avatar = data.avatar
-            console.log(edges, nodes, data)
+        data: {
+          graph: {
+            nodes: this.nodes.map((node) => {
+              const data = {
+                id: node.id,
+                type: node.data.dataType || "text",
+                content: node.data.content || "",
+                status: node.data.status,
+                created_by: node.data.created_by || "ai",
+                updated_at: node.updated_at || "",
+                created_at: node.created_at || "",
+                extra_metadata: node.extra_metadata,
+                ui_attributes: _.pick(node, [
+                  "data",
+                  "position",
+                  "class",
+                  "type",
+                ]),
+              };
+              if (node.type == "flow-form" || node.type == "flow-select") {
+                data.content = {
+                  question: node.data.question,
+                  options: node.data.options,
+                  answer: node.data.answer,
+                };
+              }
+              return data;
+            }),
+            edges: [],
+          },
         },
+      };
 
-        async saveFlow(avatar) {
-            if (avatar) {
-                this.avatar = avatar
-            }
-            const data = {
-                name: this.name,
-                ui_attributes: {
-                    avatar: this.avatar,
-                },
-                data: {
-                    graph: {
-                        nodes: this.nodes.map((node) => {
-                            const data = {
-                                id: node.id,
-                                type: node.data.dataType || "text",
-                                content: node.data.content || "",
-                                status: node.data.status,
-                                created_by: node.data.created_by || 'ai',
-                                updated_at: node.updated_at || '',
-                                created_at: node.created_at || '',
-                                extra_metadata: node.extra_metadata,
-                                ui_attributes: _.pick(node, ["data",
-                                    'position',
-                                    'class',
-                                    'type',
-                                ],)
-                            }
-                            if (node.type == "flow-form" || node.type == "flow-select") {
-                                data.content = {
-                                    question: node.data.question,
-                                    options: node.data.options,
-                                    answer: node.data.answer,
-                                }
-                            }
-                            return data
-                        }),
-                        edges: []
-                    }
-                }
-            }
+      const res = await updateWhiteBoard(this.id, data);
+    },
 
-            const res = await updateWhiteBoard(this.id, data)
-        },
+    removeRecommendNode(id) {
+      console.log("remove recommend node", id);
+      this.recommendNodes = this.recommendNodes.filter((node) => {
+        const find = node.data.id !== id;
+        console.log(find, node.data.id, id);
+        return find;
+      });
+      return true;
+    },
 
-        removeRecommendNode(id) {
-            console.log("remove recommend node", id)
-            this.recommendNodes = this.recommendNodes.filter((node) => {
-                const find = node.data.id !== id
-                console.log(find, node.data.id, id)
-                return find
-            })
-            return true
+    setProperty(config) {
+      for (const key in config) {
+        this[key] = config[key];
+      }
+    },
+    async addNode(node) {
+      this.nodes.push(node);
+      await this.saveFlow();
+    },
 
-        },
+    async removeNode(id) {
+      console.log("remove node", id);
+      this.nodes = this.nodes.filter((node) => node.id !== id);
+      await this.saveFlow();
+    },
 
-        setProperty(config) {
-            for (const key in config) {
-                this[key] = config[key]
-            }
-        },
-        async addNode(node) {
-            this.nodes.push(node)
-            await this.saveFlow()
-        },
+    addEdge(edge) {
+      this.edges.push(edge);
+    },
 
-        async removeNode(id) {
-            console.log("remove node", id)
-            this.nodes = this.nodes.filter((node) => node.id !== id)
-            await this.saveFlow()
+    async fetchNewNodes() {
+      await this.saveFlow();
+      const promises = [
+        getNewFormNodes(this.id),
+        getNewSearchNodes(this.id),
+        getNewAINodes(this.id),
+      ];
+      const [formNodes, searchNodes, aiNodes] = await Promise.all(promises);
+      console.log(formNodes, searchNodes, aiNodes);
+      this.recommendNodes = [
+        ...formNodes.map((node) => {
+          return {
+            type: node.type == "text" ? "flow-form" : "flow-select",
+            data: {
+              id: nanoid(),
+              dataType: node.type,
+              question: node.question,
+              options: node.options,
+              created_by: "ai",
+              answer: "",
+            },
+          };
+        }),
+        ...searchNodes.map((node) => {
+          return {
+            type: "bilibili-video",
+            data: {
+              id: nanoid(),
+              ...node,
+              created_by: "ai",
+            },
+          };
+        }),
+        ...aiNodes.map((node) => {
+          return {
+            type: "flow-text",
+            data: {
+              id: nanoid(),
+              dataType: "text",
+              created_by: "ai",
+              content: node,
+            },
+          };
+        }),
+      ];
+      console.log(this.recommendNodes);
+    },
 
-        },
-
-        addEdge(edge) {
-            this.edges.push(edge)
-        },
-
-        async fetchNewNodes() {
-            await this.saveFlow()
-            const promises = [getNewFormNodes(this.id), getNewSearchNodes(this.id), getNewAINodes(this.id)]
-            const [formNodes, searchNodes, aiNodes] = await Promise.all(promises)
-            console.log(formNodes, searchNodes, aiNodes)
-            this.recommendNodes = [
-                ...formNodes.map((node) => {
-                    return {
-                        type: node.type == "text" ? "flow-form" : "flow-select",
-                        data: {
-                            id: nanoid(),
-                            dataType: node.type,
-                            question: node.question,
-                            options: node.options,
-                            created_by: "ai",
-                            answer: "",
-                        },
-                    }
-                }),
-                ...searchNodes.map((node) => {
-                    return {
-                        type: "bilibili-video",
-                        data: {
-                            id: nanoid(),
-                            ...node,
-                            created_by: "ai"
-                        },
-                    }
-                }),
-                ...aiNodes.map((node) => {
-                    return {
-                        type: "flow-text",
-                        data: {
-                            id: nanoid(),
-                            dataType: "text",
-                            created_by: "ai",
-                            content: node,
-                        }
-                    }
-                })
-            ]
-            console.log(this.recommendNodes)
-        }
-    }
-})
+    async getAnswer() {
+      const answer = await getAnswer(this.id);
+      return answer;
+    },
+  },
+});
